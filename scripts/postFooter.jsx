2e2232b14772d99a715f31351d6ee6dc26626021
Nathan Vass <nathanevass@gmail.com>
2017-07-24 02:59:04 -0400

    commit

--- scripts/postFooter.jsx
+++ scripts/postFooter.jsx
@@ -8,17 +8,17 @@ const PostFooter = ({ post, hover }) => {
     "color": hover ? "#999": "#777"
   };
   return(
-        <div className="post-footer">
-          <i className="fa fa-user" aria-hidden="true"></i>&nbsp;
-          <h5 style={ textStyle }>{ `/u/${post.author}` }</h5>
-          <span>.</span>&nbsp;
-          <i className="fa fa-clock-o" aria-hidden="true"></i>
-          <h5 style={ textStyle }>{timeAgo(post.created_utc)}</h5>
-          <span>.</span>&nbsp;
-          <i className="fa fa-bolt" aria-hidden="true"></i>&nbsp;
-          <h5 style={ textStyle }>{post.num_comments}</h5>
-        </div>
-      );
+      <div className="post-footer">
+        <i className="fa fa-user" aria-hidden="true"></i>&nbsp;
+        <h5 style={ textStyle }>{ `/u/${post.author}` }</h5>
+        <span>.</span>&nbsp;
+        <i className="fa fa-clock-o" aria-hidden="true"></i>
+        <h5 style={ textStyle }>{timeAgo(post.created_utc)}</h5>
+        <span>.</span>&nbsp;
+        <i className="fa fa-bolt" aria-hidden="true"></i>&nbsp;
+        <h5 style={ textStyle }>{post.num_comments}</h5>
+      </div>
+    );
   }
 
 export default PostFooter;
